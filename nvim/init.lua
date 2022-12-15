322e3993970e222b95b48be686b19195bb2644e1
Dan Greaves <dan@dangreaves.com>
2022-12-15 18:17:37 +1100

    add link to nvim kickstart

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,6 @@
+-- source(s)
+-- https://github.com/nvim-lua/kickstart.nvim
+
 -- install packer
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 local is_bootstrap = false
